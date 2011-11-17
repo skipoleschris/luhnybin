2bc3fd4b3fd1c555dd65ac27017062c3ab6cd06f
Chris Turner <chris@templemore.co.uk>
2011-11-17 17:39:07 +0000

    Moved scala library inside the project

--- mask.sh
+++ mask.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # Call your program here instead of cat.
-java -cp target/scala-2.9.1/classes:/Users/chris/.sbt/boot/scala-2.9.1/lib/scala-library.jar templemore.luhnybin.Main
+java -cp target/scala-2.9.1/classes:lib/scala-library.jar templemore.luhnybin.Main
 
